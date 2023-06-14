3424a1cccbd9741a1039870ea7e0754c62ec27e5
rutyak <132219103+rutyak@users.noreply.github.com>
2023-06-14 15:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let sum=0;
+	let ans=0;
+	for(let i=0;i<arr.length;i++){
+		sum+=arr[i];
+		if(sum > target){
+			ans=sum;
+		}
+	}
+  return ans;
 }
 
 module.exports = threeSum;
